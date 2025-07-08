20fb8d0466401fc58127f25e62b64db577fe478e
Suresh Penkey <118256832+suresh-penkey12@users.noreply.github.com>
2025-07-08 16:24:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,20 +23,31 @@ const app = express();
 //     credentials: true
 //   };
   
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin"," https://todo-task-smart-frontend-gt7r.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
+  // app.use((req, res, next) => {
+  //   res.header("Access-Control-Allow-Origin"," https://todo-task-smart-frontend-gt7r.vercel.app");
+  //   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  //   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  //   res.header("Access-Control-Allow-Credentials", "true");
   
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(204);
-    }
+  //   if (req.method === "OPTIONS") {
+  //     return res.sendStatus(204);
+  //   }
   
-    next();
-  });
+  //   next();
+  // });
   
 // app.options(/.*/, cors(corsOptions));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://todo-task-smart-frontend-gt7r.vercel.app"
+];
+
+const corsOptions = {
+  origin: allowedOrigins,
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(morgan("dev"));
